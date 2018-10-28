99249e69a0a85d28d74bf80784573455d07952c8
WilPolanco <polanco260593@gmail.com>
2018-10-28 04:36:25 +0000

    derechos

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'cloudvps_sitioEISI');
 define('DB_USER', 'eisi_wpadmin');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'we1S1%8102p');
+define('DB_PASSWORD', 'We1S1%8102P');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
@@ -77,7 +77,8 @@ $table_prefix  = 'wp_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define( 'WP_TEMP_DIR', ABSPATH . 'wp-content/tmp/') ;
+/*define( 'WP_TEMP_DIR', ABSPATH . 'wp-content/uploads/') ;*/
+define('WP_TEMP_DIR',dirname(__FILE__).'/wp-content/uploads');
 define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
